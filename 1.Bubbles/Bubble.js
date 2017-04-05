8ec7276e141730bc663ad838c260bc1f795c1929
Ayuth Mangmesap <ayuth.mang@gmail.com>
2017-04-05 11:44:15 +0700

    Improved more efficiency of bubble

--- 1.Bubbles/Bubble.js
+++ 1.Bubbles/Bubble.js
@@ -5,26 +5,29 @@ this.Bubble = function(){
   this.r = random(5,10);
   this.g = random(5,10);
   this.MIN_SPEED = 0.1
-  this.MAX_SPEED = 1.5;
+  this.MAX_SPEED = 2.0;
+
+
   this.show = function(){
     ellipse(this.x , this.y, this.r, this.r);
   }
 
-  this.moveUp = function(){
+  this.update = function(){
+    //if bubble out of bound then we repositioning of x and y
+    if(this.y <= 0){
+      this.x = random(0, width);
+      this.y = random(0, width);
+      this.r = random(5,10);
+      this.g = random(5,10);
+    }
     this.y -= this.g;
-
+    //for random left and right to move
     if(random(0,1) < .5){
       //left
       this.x -= random(this.MIN_SPEED, this.MAX_SPEED);
     }
-    else{
+    else{ //right
       this.x += random(this.MIN_SPEED, this.MAX_SPEED);
     }
-    // this.x += random(0.1 , 5);
-  }
-
-  this.update = function(){
-    this.moveUp();
-
   }
 }
